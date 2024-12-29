817ca696c90d209013ed1099f7660e3e077c6c8e
N3m0xxx <ducl1296@gmail.com>
2024-12-29 06:47:24 -0600

    Modificaciones, borrado de titulo, se agrega calculo de liquidos al main menu

--- MdCalculator.py
+++ MdCalculator.py
@@ -7,6 +7,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
+
 st.title("🩺 MdCalculator")
 
 col1, col2, col3 = st.columns(3)
@@ -14,3 +15,6 @@ with col1:
     st.page_link("pages/BalanceHidrico.py", label="Balance Hidrico", icon="🚰")
 with col2:
     st.page_link("pages/PAM.py", label="PAM", icon="🎰") 
+with col3:
+    st.page_link("pages/calculoLiquidos.py", label="Calculo de Liquidos", icon="💦")
+
---
+++ pages/calculoLiquidos.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.page_link("MdCalculator.py",icon="🩺")
+
+st.title("💦 Calculo de Requerimiento de liquidos")
+st.write("Esta es una calculadora para calcular liquidos por peso corregido")
